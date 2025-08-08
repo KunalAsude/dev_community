029f44c4a581c8ce84184655731196e99357e978
Kunal Asude <kunalasude@gmail.com>
2025-08-08 23:40:53 +0530

    Added topic model and updated room views

--- base/migrations/0001_initial.py
+++ base/migrations/0001_initial.py
@@ -12,7 +12,7 @@ class Migration(migrations.Migration):
 
     operations = [
         migrations.CreateModel(
-            name='room',
+            name='Room',
             fields=[
                 ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('name', models.CharField(max_length=200)),
--- base/views.py
+++ base/views.py
@@ -1,22 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Room
 
-rooms = [
-    {"id": 1, "name": "Python", "likes": "Let’s learn Python from basics to advanced"},
-    {"id": 2, "name": "JavaScript", "likes": "Learn JS and build interactive websites"},
-    {"id": 3, "name": "Django", "likes": "Master Django for full-stack web development"}
-]
+# rooms = [
+#     {"id": 1, "name": "Python", "likes": "Let’s learn Python from basics to advanced"},
+#     {"id": 2, "name": "JavaScript", "likes": "Learn JS and build interactive websites"},
+#     {"id": 3, "name": "Django", "likes": "Master Django for full-stack web development"}
+# ]
 
 
 def home(req):
+    rooms=Room.objects.all()
     context = {'rooms':rooms}
     return render(req,'base/home.html',context)
 
 def room(req,pk):
-    room = None
-    for i in rooms:
-        if i['id']==int(pk):
-            room=i
+    room = Room.objects.get(id=pk)
 
     context={'room':room}
     
